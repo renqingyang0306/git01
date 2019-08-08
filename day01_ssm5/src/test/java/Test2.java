c5242410be7172f31664a039dde9046f72ea90b7
ren970306 <15514267206@163.com>
2019-08-08 11:29:46 +0800

    rqy 添加测试功能

---
+++ day01_ssm5/src/test/java/Test2.java
@@ -0,0 +1,7 @@
+/**
+ * @author 任清阳
+ * @Email 1277409109@qq.com
+ * @date 2019/8/8 11:22
+ */
+public class Test2 {
+}
---
+++ day01_ssm5/src/test/java/test.java
@@ -0,0 +1,7 @@
+/**
+ * @author 任清阳
+ * @Email 1277409109@qq.com
+ * @date 2019/8/7 23:54
+ */
+public class test {
+}
